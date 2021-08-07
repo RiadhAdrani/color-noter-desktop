e861c10193b31c44bfcc7d9450b1db03d0b32502
adrani_laptop <riadh_adrani@hotmail.fr>
2021-08-07 14:45:02 +0100

    added react

---
+++ public/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+require("@electron/remote/main").initialize();
+
+function createWindow() {
+     const win = new BrowserWindow({
+          minWidth: 600,
+          minHeight: 800,
+          webPreferences: {
+               enableRemoteModule: true,
+          },
+     });
+
+     win.loadURL("http://localhost:3000");
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+     if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
